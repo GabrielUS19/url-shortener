b91ced0a974053dc9e5732a6b25b4509703ac37f
Gabriel Uchoa <gabrieluchoa2607@gmail.com>
2026-02-08 08:51:28 -0300

    feat: Implements the HttpStatus Enum

---
+++ src/main/java/com/gabriel/urlshortener/network/HttpStatus.java
@@ -0,0 +1,25 @@
+package com.gabriel.urlshortener.network;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    SERVER_INTERNAL_ERROR(500, "Server Internal Error");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
